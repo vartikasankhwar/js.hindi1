2b8668b21bebca4f9e78e62ca98444fabfc81902
vartikasankhwar <2023aspire99@gmail.com>
2024-11-09 10:57:54 +0000

    datatypes and variables

---
+++ Basics/L4_variables.js
@@ -0,0 +1,17 @@
+const accountId=144553 //Iski value change nhi kr skte as hmne neeche walo ki ki hai
+
+//js me variable declare krne ke 2 ways hai 1.let 2.var
+//But we'll use "let" for variable declaration
+let accountEmail="vartika@google.com"
+var accountPassword="12345"
+
+accountCity="Kanpur" //Koi variable aise bhi declare ho skta haii but it is not a good way and it can create many complexities
+
+let accountState; //Agar js me koi value initialize nhi krte to js usse "UNDEFINED" manti hai
+
+
+accountEmail="vartika@gmail.com"
+accountPassword="8760"
+accountCity="delhi"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);

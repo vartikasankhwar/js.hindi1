db82e665ec029caa7289325bb911afcf46d66601
vartikasankhwar <2023aspire99@gmail.com>
2024-11-09 11:11:08 +0000

    container

---
+++ Basics/L7.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3) //It will not work because we are using nodejs not browser
+
+console.log(3+3)
+
+console.log("vartika")
+
+
+/*
+datatypes==
+
+1. number=> 2 to power 53
+2. string => " "
+3. bigint => for very large no.
+4. boolean => true/false
+5. null
+6. unidentified => when it is written as "var name;"
+7. symbol => unique
+8. object 
+*/
+
+console.log(typeof "vartika" ) //kisi bhi cheez ka type janne ke liye "typeof" ka use krte hai
+
+console.log(typeof null) //output=object
+console.log(typeof undefined) //output=undefined

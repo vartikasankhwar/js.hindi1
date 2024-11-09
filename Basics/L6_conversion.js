de239791a24fe15355e34f2cac79faf7c5f5f3a0
vartikasankhwar <2023aspire99@gmail.com>
2024-11-09 10:53:53 +0000

    conversion

---
+++ Basics/L6_conversion.js
@@ -0,0 +1,46 @@
+//number
+let score1=22
+console.log(typeof score)
+
+//string
+let score2="22abs"
+console.log(typeof score2)
+
+
+
+
+let value2=Number(score2)
+console.log(typeof value2);
+console.log(value2)
+
+//Null value
+let score3=null
+let value3=Number(score3)
+console.log(typeof value3)
+console.log(value3)
+
+//undefined
+let score4=undefined
+let value4=Number(score4)
+console.log(typeof value4);
+console.log(value4)
+
+//boolean value
+let score5=true
+let value5=Number(score5)
+console.log(typeof value5);
+console.log(value5)
+console.log(typeof score5);
+
+
+//true/false
+let score6=1
+let value6=Boolean(score6)
+console.log(value6);
+
+//No. to string conversion
+let score7=33
+let value7=String(score7)
+console.log(value7);
+console.log(typeof value7);
+console.log("vartika")
